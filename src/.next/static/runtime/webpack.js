f798d67f0b71256015dba63713043df61a4ed127
Wisdom Kwarteng <kwartengwisdomug95@gmail.com>
2019-02-19 23:12:12 +0000

    modifying design

--- src/.next/static/runtime/webpack.js
+++ src/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "831dbec101989d72363b";
+/******/ 	var hotCurrentHash = "e21714ea1cf3799b1ea9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
